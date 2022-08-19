feecbf70c64f044a04ca027d61d89cd637fd774e
Katherine Chiu <kachiu@ucdavis.edu>
2022-08-19 10:07:23 -0700

    updated

--- subset_df.py
+++ subset_df.py
@@ -1,17 +1,21 @@
-def subset_df(df1, df2):
+def subset_df(df1, df2, col="ID", complement=False):
     # Set index
-    df1 = df1.set_index("ID")
-    df2 = df2.set_index("ID")
+    df1 = df1.set_index(col)
+    df2 = df2.set_index(col)
     # Find common IDs
     intersection = df1.join(df2, how="inner", rsuffix="1")
     intersection = intersection.reset_index()
-    IDs = intersection["ID"].to_list()
+    IDs = intersection[col].to_list()
     # Subset both dfs to only include common IDs
     df1 = df1.reset_index()
-    df1 = df1[df1['ID'].isin(IDs)]
     df2 = df2.reset_index()
-    df2 = df2[df2['ID'].isin(IDs)]
+    if complement == True:
+        df1 = df1[-df1[col].isin(IDs)]
+        df2 = df2[-df2[col].isin(IDs)]
+    else:
+        df1 = df1[df1[col].isin(IDs)]
+        df2 = df2[df2[col].isin(IDs)]
     # Sort
-    df1 = df1.sort_values(by="ID")
-    df2 = df2.sort_values(by="ID")
+    df1 = df1.sort_values(by=col)
+    df2 = df2.sort_values(by=col)
     return (df1, df2)
